8b3f5959fba809abcdb2b7870de4f17ce6b5a5e4
Angelo Maynigo <angelomaynigo@gmail.com>
2026-07-22 05:37:31 -0700

    Perfect Dark: fixed issue with weighted options breaking logic for Universal Tracker

--- worlds/perfect_dark/world.py
+++ worlds/perfect_dark/world.py
@@ -19,6 +19,30 @@ class PerfectDarkWorld(World):
 
     origin_region_name = "Carrington Institute"
 
+    def generate_early(self) -> None:
+        if hasattr(self.multiworld, "re_gen_passthrough"):
+            if "Perfect Dark" in self.multiworld.re_gen_passthrough:
+                passthrough = self.multiworld.re_gen_passthrough["Perfect Dark"]
+
+                # Sync options for weighted values
+                self.options.goal.value = passthrough["options"]["goal"]
+                self.options.skedar_ruins_requirements.value = passthrough["options"]["skedar_ruins_requirements"]
+                self.options.mission_logic.value = passthrough["options"]["mission_logic"]
+                self.options.agent.value = passthrough["options"]["agent"]
+                self.options.required_agent_mission_stars.value = ["options"]["agent_mission_stars"]
+                self.options.special_agent.value = passthrough["options"]["special_agent"]
+                self.options.required_special_agent_mission_stars.value = ["options"]["special_agent_mission_stars"]
+                self.options.perfect_agent.value = passthrough["options"]["perfect_agent"]
+                self.options.required_perfect_agent_mission_stars.value = ["options"]["perfect_agent_mission_stars"]
+                self.options.weapon_progression.value = passthrough["options"]["weapon_progression"]
+                self.options.challenges.value = passthrough["options"]["challenges"]
+                self.options.required_challenge_stars.value = passthrough["options"]["required_challenge_stars"]
+                self.options.challenge_logic.value = passthrough["options"]["challenge_logic"]
+                self.options.weapon_training.value = passthrough["options"]["weapon_training"]
+                self.options.device_training.value = passthrough["options"]["device_training"]
+                self.options.holotraining.value = passthrough["options"]["holotraining"]
+                self.options.unlock_cheats.value = passthrough["options"]["unlock_cheats"]
+
     def create_regions(self) -> None:
         regions.create_and_connect_regions(self)
         locations.create_all_locations(self)
@@ -35,24 +59,23 @@ class PerfectDarkWorld(World):
     def get_filler_item_name(self) -> str:
         return items.get_random_filler_item_name(self)
 
-    def fill_slot_data(self) -> Mapping[str, Any]:
-        required_mission_stars = 0
-        if self.options.agent:
-            required_mission_stars += self.options.required_agent_mission_stars.value
-        if self.options.special_agent:
-            required_mission_stars += self.options.required_special_agent_mission_stars.value
-        if self.options.perfect_agent:
-            required_mission_stars += self.options.required_perfect_agent_mission_stars.value
-
-        slot_data = {
+    def fill_slot_data(self) -> dict[str, Any]:
+        slot_data: dict[str, Any] = {
             "options": {
                 "goal": self.options.goal.value,
                 "skedar_ruins_requirements": self.options.skedar_ruins_requirements.value,
-                "required_mission_stars": required_mission_stars,
+                "mission_logic": self.options.mission_logic.value,
+                "agent": self.options.agent.value,
+                "agent_mission_stars": self.options.required_agent_mission_stars.value,
+                "special_agent": self.options.special_agent.value,
+                "special_agent_mission_stars": self.options.required_special_agent_mission_stars.value,
+                "perfect_agent": self.options.perfect_agent.value,
+                "perfect_agent_mission_stars": self.options.required_perfect_agent_mission_stars.value,
                 "weapon_progression": self.options.weapon_progression.value,
                 "allow_progressive_weapon_in_challenges": self.options.allow_progressive_weapon_in_challenges.value,
                 "challenges": self.options.challenges.value,
                 "required_challenge_stars": self.options.required_challenge_stars.value,
+                "challenge_logic": self.options.challenge_logic.value,
                 "shorter_challenges": self.options.shorter_challenges.value,
                 "weapon_training": self.options.weapon_training.value,
                 "device_training": self.options.device_training.value,
@@ -62,4 +85,8 @@ class PerfectDarkWorld(World):
             },
         }
 
+        return slot_data
+
+    @staticmethod
+    def interpret_slot_data(slot_data: dict[str, Any]) -> dict[str, Any]:
         return slot_data
